9643c2dcb5e72b1cf49f58ab2e39724514c30aa5
dosepehr <dosepehr@gmail.com>
2023-07-27 00:40:37 +0330

    add CustomObject

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Starter from './components/Starter';
+import CustomObject from './components/CustomObject';
 
 const App = () => {
     return (
         <>
+            <CustomObject />
             <Starter />
         </>
     );
 };
 
 export default App;
+

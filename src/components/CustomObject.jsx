58389770db7584f08929e086aa56b5f7e31f20d8
dosepehr <dosepehr@gmail.com>
2023-07-27 00:47:23 +0330

    memoize Custom Object

--- src/components/CustomObject.jsx
+++ src/components/CustomObject.jsx
@@ -1,10 +1,14 @@
 import { DoubleSide } from 'three';
+import { useMemo } from 'react';
 const CustomObject = () => {
     const verticesCount = 10 * 3;
-    const positions = new Float32Array(verticesCount * 3);
-    for (let i = 0; i < verticesCount * 3; i++) {
-        positions[i] = (Math.random() - 0.5) * 3;
-    }
+    const positions = useMemo(() => {
+        const array = new Float32Array(verticesCount * 3);
+        for (let i = 0; i < verticesCount * 3; i++) {
+            array[i] = (Math.random() - 0.5) * 3;
+        }
+        return array;
+    }, [verticesCount]);
 
     return (
         <mesh>

9bfc2da0e07df62678089404a06d3f689bbfa22a
dosepehr <dosepehr@gmail.com>
2023-07-27 21:41:59 +0330

    add stats component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Starter from './components/Starter';
 import CustomObject from './components/CustomObject';
-
+import Stats from './components/Stats';
 const App = () => {
     return (
         <>
+            <Stats />
             {/* <CustomObject /> */}
             <Starter />
         </>
---
+++ src/components/Stats.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Perf } from 'r3f-perf';
+
+const Stats = () => {
+    return (
+        <>
+            <Perf position='top-left'/>
+        </>
+    );
+};
+
+export default Stats;
